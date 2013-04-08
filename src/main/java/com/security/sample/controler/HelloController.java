925a8e4c753a9caf55e30a4f38c58a76abe4f445
vijitha epa <vijithaepa@gmail.com>
2013-04-08 12:34:26 +1000

    first commit

---
+++ src/main/java/com/security/sample/controler/HelloController.java
@@ -0,0 +1,27 @@
+package com.security.sample.controler;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * 
+ * @author vijitha Epa
+ * @since 26-09-12.
+ * 
+ * Simple Controller class to handle incoming requests.
+ */
+@Controller
+@RequestMapping("/welcome")
+public class HelloController {
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String printWelcome(ModelMap model) {
+
+		model.addAttribute("message", "Spring Security with Basic Authentication");
+		return "hello";
+
+	}
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+<h2>Welcome to the Spring security !</h2> </br>
+<h2>This application demonstrate Basic Authentication...</h2>
+</body>
+</html>
